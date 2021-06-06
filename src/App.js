63839d758a3a0eae52537903168c377bdd9ce3c2
Cristian bastias <cristian.bastias@gmail.com>
2021-06-06 09:42:02 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi there
-        </p>
+         Hi there d!!
+         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
