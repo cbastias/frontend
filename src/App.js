efe4b116600b54f1575641c99c4de41bdba74ae9
Cristian bastias <cristian.bastias@gmail.com>
2021-06-06 14:59:31 -0400

    App change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi there Cristian!!
+          Hi there Cristian!!
          </p>
         <a
           className="App-link"
